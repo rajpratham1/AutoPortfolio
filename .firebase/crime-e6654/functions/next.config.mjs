c62629b0416d2b4a5e297ab1f08dc99b6ec32126
rajpratham1 <188737331+rajpratham1@users.noreply.github.com>
2025-11-29 21:49:22 +0530

    Initial commit

---
+++ .firebase/crime-e6654/functions/next.config.mjs
@@ -0,0 +1,12 @@
+// auto-portfolio/next.config.mjs
+var nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  }
+};
+var next_config_default = nextConfig;
+export {
+  next_config_default as default
+};
